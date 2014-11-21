17684a5d42fc957760347e5311e8517fb167ef23
griffint <griffin.tschurwald@students.olin.edu>
2014-11-20 23:40:44 -0500

    better code

--- arduinoFIles/Sprint3Code/Sprint3Code.ino
+++ arduinoFIles/Sprint3Code/Sprint3Code.ino
@@ -8,17 +8,21 @@ Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 //code for motor shield stepper motor here
 
 //also need code to initialize input ports for switches
+int platformStep = 7;
 
 
 void setup() {
   AFMS.begin();
   myMotor->setSpeed(200);
   Serial.begin(9600);
+
+  pinMode(platformStep,OUTPUT);
 }
 
 
 void loop() {
-  
+  turnPlatform(200,0);
+  delay(2000);
   //motor check code
   
   //button press code here
@@ -54,7 +58,12 @@ void turnlinearStepper(int steps, int directions){
 void turnPlatform(int steps, int directions){
   //directions will be 0 or 1
   //this motor will use the pololu driver, so will need to use individual steps
-  
+  //pin 7 for step 8 for direction
+ int stepCounter = steps;
+  while (stepCounter>0){
+    digitalWrite(platformStep, HIGH);
+    stepCounter -= 1;
+  }
 }
   
   
